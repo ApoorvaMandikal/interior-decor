dc950298c958897ae28613eb4abea9603f2ff1a7
Apoorva Mandikal <apoorvagirish76@gmail.com>
2024-03-28 18:13:54 +0530

    Section1 completed

--- src/Components/Section1/Section1.jsx
+++ src/Components/Section1/Section1.jsx
@@ -4,35 +4,33 @@ import image1 from "./../../assets/Images/image1.png";
 const Section1 = () => {
   return (
     <>
-      <div className="my-6 items-center mx-auto" style={{ width: "85%" }}>
+      <div className="my-6 items-center mx-auto md:w-[85%] w-full">
         <div
-          className="lg:w-6/12 right-0 absolute h-52"
+          className="lg:w-6/12 right-0 lg:top-56 absolute h-52"
           style={{ backgroundColor: "#FAF4EF" }}
         ></div>
-        <div className="w-full">
+        <div className="w-full md:block flex flex-wrap relative md:ml-0 ml-[25%]">
           <img
             src={image1}
             alt="First image"
-            className="float-end"
-            style={{ width: "80%" }}
+            className="float-end md:w-[80%] w-full"
           ></img>
         </div>
-        <div
-          className="bg-white box-border z-10 relative opacity-90"
-          style={{ top: "23rem", width: "35%", height: "28rem" }}
-        >
-          <div className="w-full flex flex-col absolute mt-12 text-start ">
+        <div className=" box-border md:relative absolute h-auto md:ml-0 ml-[7%] md:w-[35%] w-[55%]">
+          <div className="bg-white z-10 opacity-90 w-full flex flex-col absolute md:mt-[70%] mt-[-100%] text-start">
             <span
-              className="block font-black py-12"
+              className="block font-black lg:py-12 md:py-6 py-2 md:text-[4vw] text-[7vw]"
               style={{
                 fontFamily: "'DM Serif Display', serif",
-                fontSize: "4rem",
                 lineHeight: "1",
               }}
             >
               Modern Interior for your Dream House
             </span>
-            <span className="text-2xl pt-2 w-9/12" style={{ color: "#737373" }}>
+            <span
+              className="md:text-[1.5vw] text-[2.5vw] my-8 w-9/12"
+              style={{ color: "#737373" }}
+            >
               We custom make designs to suit your needs.
             </span>
           </div>
