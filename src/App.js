b1420da0df562c01d58e39587a119e997dc8a3d1
Apoorva Mandikal <apoorvagirish76@gmail.com>
2024-03-30 06:02:43 +0530

    Done with section4

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./Components/Header/Header";
 import Section1 from "./Components/Section1/Section1";
 import Section2 from "./Components/Section2/Section2";
 import Section3 from "./Components/Section3/Section3";
+import Section4 from "./Components/Section4/Section4";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Section1></Section1>
       <Section2></Section2>
       <Section3></Section3>
+      <Section4></Section4>
     </div>
   );
 }
